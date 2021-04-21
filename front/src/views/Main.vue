f8e591ac1adeecfbddb857e5fccb471bd1c4313f
HwangYoungJun <dudghkd4512@gmail.com>
2021-04-21 23:06:33 +0900

    init2

---
+++ front/src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+
+
+});
+</script>
